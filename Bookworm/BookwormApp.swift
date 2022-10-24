06d9182941a55564835b950c59652938e95afaa8
Jonas Agnezi <jonas.agnezi@gmail.com>
2022-10-24 20:34:16 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,18 @@
+//
+//
+// CupcakeCorner
+// Created by: itsjagnezi on 21/10/22
+// Copyright (c) today and beyond
+//
+
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
